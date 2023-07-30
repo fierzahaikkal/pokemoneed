872d9eefa37cb40b4a2717b4975ef086e504dd48
fierzahaikkal <ojogames.id@gmail.com>
2023-07-31 00:35:42 +0700

    feat: add function to displaying data

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export const Search = () => {
   return (
-    <div className="mb-3">
+    <div className="mb-8">
       <div className="relative mb-4 flex w-full flex-wrap items-stretch">
         <input
           type="search"
-          className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-indigo-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-indigo-600 dark:text-indigo-200 dark:placeholder:text-indigo-200 dark:focus:border-primary"
-          placeholder="Search"
+          className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-indigo-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none border-indigo-600 text-indigo-200 placeholder:text-indigo-300 focus:border-primary"
+          placeholder="Search pokemon name..."
           aria-label="Search"
           aria-describedby="button-addon1"
         />
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,23 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export const Table = () => {
-  const URI = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
-  const getAPI = () => {
+  const URI = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
     axios
       .get(URI)
       .then((res) => {
-        console.log(res);
+        for (let i = 0; i < res.data.results.length; i++) {
+          setDatas(res.data.results);
+        }
       })
       .catch((err) => console.log(err));
   };
 
   return (
     <div>
-      <button
-        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onClick={getAPI}
-      >
+      <div className="relative overflow-x-auto shadow-md rounded mb-5">
+        <table className="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400 border border-indigo-600">
+          <thead className="text-xs text-white uppercase bg-indigo-600 ">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Index
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Pokemon
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Detail
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {datas.map((data) => (
+              <tr
+                className="bg-transparent border border-indigo-600 hover:bg-gray-100"
+                key={data.name}
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  {datas.indexOf(data)}
+                </th>
+                <td className="px-6 py-4 text-indigo-600">{data.name}</td>
+                <td className="px-6 py-4">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >
+                    {data.url}
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <button className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
         Get Data
       </button>
     </div>
